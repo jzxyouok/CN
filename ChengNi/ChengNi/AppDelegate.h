ffb4552003f5fc7e20ba0fb5f5caa122b4814da7
peng <xpdragon1993@163.com>
2016-07-04 08:14:59 +0800

    初始化项目

---
+++ ChengNi/ChengNi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChengNi
+//
+//  Created by dragon on 16/7/4.
+//  Copyright © 2016年 win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
